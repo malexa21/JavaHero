3491808e80d46bc5d6a0201e1bcd31c78f729ba3
malexa21 <aaliroalexandra@gmail.com>
2023-08-23 16:04:44 +0300

    Am updatat butoanele

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ const arc = document.getElementById("arc");
 const toiag = document.getElementById("toiag");
 const shuriken = document.getElementById("shuriken");
 
+sabie.style.display = "none";
+arc.style.display = "none";
+toiag.style.display = "none";
+shuriken.style.display = "none";
 
 
 da.addEventListener("click" , function(){
     da.style.display = "none";
     nu.style.display = "none";
     mesajDa.innerHTML= "Incantator calatorule! Haide sa iti prezint armele pe care le poti alege!";
+    sabie.style.display = "block";
+    arc.style.display = "block";
+    toiag.style.display = "block";
+    shuriken.style.display = "block";
 });
 
 nu.addEventListener("click" , function(){
